dec8bda1492f690134b50c9c7161610c49cb3893
ishita <ishitajain3175@gmail.com>
2025-07-06 23:28:48 +0530

    Update CheckBox component

--- src/components/CheckBox.js
+++ src/components/CheckBox.js
@@ -102,7 +102,7 @@ const CheckBox = () => {
         };
 
         try {
-            const response = await fetch('https://disease-predicton-and-diagnosis-machine.onrender.com', {
+            const response = await fetch('https://disease-prediction-and-diagnosis-machine.onrender.com', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
